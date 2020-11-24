3cd7994e0f34922f74d61d57c0592239615e5897
Jun Tian <tianjun.cpp@gmail.com>
2020-11-24 22:31:56 +0800

    init

--- README.md
+++ README.md
@@ -1,3 +1,77 @@
 # CircularArrayBuffers
 
 [![Build Status](https://github.com/JuliaReinforcementLearning/CircularArrayBuffers.jl/workflows/CI/badge.svg)](https://github.com/JuliaReinforcementLearning/CircularArrayBuffers.jl/actions)
+
+`CircularArrayBuffers.jl` is a small package to wrap an `AbstractArray` as a buffer along the last dimension. The main benefit compared to [`CircularBuffer`](https://juliacollections.github.io/DataStructures.jl/latest/circ_buffer/) in [`DataStructures.jl`](https://github.com/JuliaCollections/DataStructures.jl) is that the view of consecutive elements is a `SubArray`.
+
+## Usage
+
+```julia
+julia> using CircularArrayBuffers
+
+julia> names(CircularArrayBuffers)
+5-element Array{Symbol,1}:
+ :CircularArrayBuffer
+ :CircularArrayBuffers
+ :CircularVectorBuffer
+ :capacity
+ :isfull
+
+julia> a = CircularArrayBuffer(rand(2,3))
+2×3 CircularArrayBuffer{Float64,2}:
+ 0.0510714  0.0260738  0.0245707
+ 0.856257   0.571643   0.0189365
+
+julia> b = CircularArrayBuffer{Float64}(2,3)
+2×0 CircularArrayBuffer{Float64,2}
+
+julia> push!(b, rand(2))
+2×1 CircularArrayBuffer{Float64,2}:
+ 0.4215856115651755
+ 0.5485806794787502
+
+julia> push!(b, rand(2))
+2×2 CircularArrayBuffer{Float64,2}:
+ 0.421586  0.640501
+ 0.548581  0.774729
+
+julia> push!(b, rand(2))
+2×3 CircularArrayBuffer{Float64,2}:
+ 0.421586  0.640501  0.653054
+ 0.548581  0.774729  0.902611
+
+julia> push!(b, rand(2))
+2×3 CircularArrayBuffer{Float64,2}:
+ 0.640501  0.653054  0.640373
+ 0.774729  0.902611  0.227435
+
+julia> pop!(b)
+2-element view(::CircularArrayBuffer{Float64,2}, :, 3) with eltype Float64:
+ 0.6403725468830439
+ 0.22743495787074597
+
+julia> b
+2×2 CircularArrayBuffer{Float64,2}:
+ 0.640501  0.653054
+ 0.774729  0.902611
+
+julia> size(b)
+(2, 2)
+
+julia> capacity(b)
+3
+
+julia> isfull(b)
+false
+
+julia> push!(b, rand(2))
+2×3 CircularArrayBuffer{Float64,2}:
+ 0.640501  0.653054  0.885887
+ 0.774729  0.902611  0.0332439
+
+julia> isfull(b)
+true
+
+julia> eltype(b)
+Float64
+```
--- src/CircularArrayBuffers.jl
+++ src/CircularArrayBuffers.jl
@@ -1,5 +1,82 @@
 module CircularArrayBuffers
 
-# Write your package code here.
+export CircularArrayBuffer, CircularVectorBuffer, capacity, isfull
+
+"""
+    CircularArrayBuffer{T}(sz::Integer...) -> CircularArrayBuffer{T, N}
+
+`CircularArrayBuffer` uses a `N`-dimension `Array` of size `sz` to serve as a buffer for
+`N-1`-dimension `Array`s of the same size.
+"""
+mutable struct CircularArrayBuffer{T,N} <: AbstractArray{T,N}
+    buffer::Array{T,N}
+    first::Int
+    nframes::Int
+    step_size::Int
+end
+
+const CircularVectorBuffer{T} = CircularArrayBuffer{T, 1}
+
+function CircularArrayBuffer{T}(d::Integer...) where {T}
+    N = length(d)
+    CircularArrayBuffer(Array{T}(undef, d...), 1, 0, N == 1 ? 1 : *(d[1:end-1]...))
+end
+
+function CircularArrayBuffer(A::AbstractArray{T,N}) where {T,N}
+    CircularArrayBuffer(A, 1, size(A, N), N == 1 ? 1 : *(size(A)[1:end-1]...))
+end
+
+Base.IndexStyle(::CircularArrayBuffer) = IndexLinear()
+
+Base.size(cb::CircularArrayBuffer{T,N}, i::Integer) where {T,N} = i == N ? cb.nframes : size(cb.buffer, i)
+Base.size(cb::CircularArrayBuffer{T,N}) where {T,N} = ntuple(i -> size(cb, i), N)
+Base.getindex(cb::CircularArrayBuffer{T,N}, i::Int) where {T,N} = getindex(cb.buffer, _buffer_index(cb, i))
+Base.setindex!(cb::CircularArrayBuffer{T,N}, v, i::Int) where {T,N} = setindex!(cb.buffer, v, _buffer_index(cb, i))
+
+capacity(cb::CircularArrayBuffer{T,N}) where {T,N} = size(cb.buffer, N)
+isfull(cb::CircularArrayBuffer) = cb.nframes == capacity(cb)
+Base.isempty(cb::CircularArrayBuffer) = cb.nframes == 0
+
+@inline function _buffer_index(cb::CircularArrayBuffer, i::Int)
+    ind = (cb.first - 1) * cb.step_size + i
+    mod1(ind, length(cb.buffer))
+end
+
+@inline function _buffer_frame(cb::CircularArrayBuffer, i::Int)
+    n = capacity(cb)
+    idx = cb.first + i - 1
+    mod1(idx, n)
+end
+
+_buffer_frame(cb::CircularArrayBuffer, I::Vector{Int}) = map(i -> _buffer_frame(cb, i), I)
+
+function Base.empty!(cb::CircularArrayBuffer)
+    cb.nframes = 0
+    cb
+end
+
+function Base.push!(cb::CircularArrayBuffer{T, N}, data) where {T,N}
+    if cb.nframes == capacity(cb)
+        cb.first = (cb.first == capacity(cb) ? 1 : cb.first + 1)
+    else
+        cb.nframes += 1
+    end
+    if N == 1
+        cb[cb.nframes] = data
+    else
+        cb[ntuple(_ -> (:), N - 1)..., cb.nframes] .= data
+    end
+    cb
+end
+
+function Base.pop!(cb::CircularArrayBuffer{T, N}) where {T,N}
+    if cb.nframes <= 0
+        throw(ArgumentError("buffer must be non-empty"))
+    else
+        res = @views cb[ntuple(_ -> (:), N - 1)..., cb.nframes]
+        cb.nframes -= 1
+        res
+    end
+end
 
 end
